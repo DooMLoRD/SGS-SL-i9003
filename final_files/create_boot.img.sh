c18d303be47d4ce65d3dc3717a30dfbb8dbc9c5f
DooMLoRD <metoo.mememe@gmail.com>
2011-11-22 02:22:58 +0530

    added build tools

---
+++ final_files/create_boot.img.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Kernel packer for GT-I9003 by skin1980
+# slightly modified for our build structure
+
+if test -e zImage
+then
+    ./mkbootimg --kernel zImage --ramdisk newramdisk.cpio.gz --pagesize 1000 -o normalboot.img 
+    tar -cvf new-GT-I9003_PDA.tar normalboot.img 
+else
+    echo "No zImage found"
+fi
+
---
+++ ramdisks/WIP/sbin/boot/properties.sh
@@ -0,0 +1,11 @@
+#setprop customkernel.cf-root true
+#setprop customkernel.base.cf-root true
+#setprop customkernel.name CF-Root-I9003
+#setprop customkernel.namedisplay "CF-Root-I9003 4.3"
+#setprop customkernel.version.number 82
+#setprop customkernel.version.name 4.3
+propset customkernel.cf-root true
+propset customkernel.name CF-Root-I9003
+propset customkernel.namedisplay "DooMKernel with CF-Root-I9003 by amit.bagaria"
+propset customkernel.version.number 82
+propset customkernel.version.name 4.3
