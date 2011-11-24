04208b0ad7333020de1fff28e8cfb56a05b992e8
DooMLoRD <metoo.mememe@gmail.com>
2011-11-24 23:07:24 +0530

    removed unused files

--- ramdisks/CFROOT-WIP/sbin/bootanimation.sh
+++
@@ -1,9 +0,0 @@
-#!/sbin/ext/busybox sh
-
-if [ -f /data/local/bootanimation.bin ]; then
-  /data/local/bootanimation.bin
-elif [ -f /data/local/bootanimation.zip ] || [ -f /system/media/bootanimation.zip ]; then
-  /system/bin/bootanimation
-else
-  /system/bin/samsungani
-fi;
